a3d91cd6cac95bfe11b3b0d1d0b588368da44cce
MyDreamIsDevelo <91327165+MyDreamIsDeveloper@users.noreply.github.com>
2021-10-01 02:57:16 +0000

    Class 기초

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
-# dict 연습
+# 클래스 생성
 
-x = dict()
-y = {}
+class Person:
+  def __init__(self, name, age): #init = initialize
+    self.name = name
+    self.age = age
 
-print(x)
-print(y)
+# self는 this와 같은 기능 같음
+  def say_hello(self, to_name):
+    print("안녕 " + to_name + ", 나는 " + self.name)
 
-x = {
-  "name": "철수",
-  "age": 20,
-}
+  def status(self):
+    print("이름:" + self.name + " / 나이:" + str(self.age))
 
-# dict는 Key와 Value로 구성, Map과 유사한 기능으로 보임
-print(x["name"])
-print(x["age"])
+A = Person("A", 10)
+B = Person("B", 20)
+C = Person("C", 30)
 
-print(x.keys()) # x의 Key 전체 출력
-print(x.values()) # x의 Value 전체 출력
+A.say_hello("철수")
+B.say_hello("영희")
+C.say_hello("미영")
+print("")
+A.status()
+B.status()
+C.status()
 
-for n in x: # dict의 모든 key-value 출력
-  print(n + ":" + str(x[n]))
+# 클래스 상속
+class Police(Person):
+  def arrest(self, to_arrest):
+    print("체포:" + to_arrest)
+
+class Programmer(Person):
+  def program(self, to_program):
+    print("프로그래밍:" + to_program)
+
+A = Person("A", 10)
+B = Police("B", 20)
+C = Programmer("C", 30)
